109c71180db3ef865499bc00d78b76e4c4c3f424
Anatolii-Borshch <anatoliiborshch@gmail.com>
2025-09-01 22:44:50 +0300

    [Fix] Fixed comment deletion while deleting comment post.

--- src/Camply.Server/Camply.Persistence/Repositories/CommentRepository.cs
+++ src/Camply.Server/Camply.Persistence/Repositories/CommentRepository.cs
@@ -11,24 +11,26 @@ namespace Camply.Persistence.Repositories
         {
         }
 
-        public async Task<IEnumerable<Comment>> GetCommentsByPostIdAsync(Guid postId, int numberOfComments)
-        {
-            return await _context.Comments
-                .Include(x => x.User)
-                .Include(x => x.Replies)
-                .ThenInclude(x => x.User)
-                .Where(x => x.PostId == postId && x.ParentCommentId == null)
-                .OrderByDescending(c => c.CreatedDate)
-                .Take(numberOfComments)
-                .ToListAsync();
-        }
+            public async Task<IEnumerable<Comment>> GetCommentsByPostIdAsync(Guid postId, int numberOfComments)
+            {
+                var comments = await _context.Comments
+                    .Where(c => c.PostId == postId && c.ParentCommentId == null)
+                    .Include(c => c.User)
+                    .Include(c => c.Replies)
+                        .ThenInclude(r => r.User)
+                    .OrderByDescending(c => c.CreatedDate)
+                    .Take(numberOfComments)
+                    .ToListAsync();
+
+                return comments;
+            }
 
         public async Task<Comment?> GetByIdWithRepliesAsync(Guid id)
         {
             return await _context.Comments
-                .Include(x => x.UserId)
+                .Include(x => x.User)
                 .Include(x => x.Replies)
-                .ThenInclude(x => _context.Users)
+                    .ThenInclude(x => x.User)
                 .FirstOrDefaultAsync(x => x.Id == id);
         }
     }
